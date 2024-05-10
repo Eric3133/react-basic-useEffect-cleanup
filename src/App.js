7d1ae805447fc48281ecdc234c09496c1a1866d8
mahongri <mahongri1993@gmail.com>
2024-05-09 21:14:37 -0400

    useEffect cleanup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from 'react';
+
+// useEffect cleaning up
+
+function Son ()  {
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      console.log('This is Son')
+    }, 1000)
+
+    return () => {
+      clearInterval(timer)
+    }
+  },[])
+
+  return (
+    <div>
+      <h1>This is Son</h1>
+    </div>
+
+  )
+
+}
 
 function App() {
+  const [show, setShow] = useState(true);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {show && <Son />}
+      <button onClick = {() => setShow(false)}>unstall Son</button>
+
     </div>
   );
 }
